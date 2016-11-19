e3965ee86ccf57eca1aab05261a12135c75e22dd
eliot-akira <me@eliotakira.com>
2016-11-19 04:23:59 +0100

    search babel presets path

--- src/babel.config.js
+++ src/babel.config.js
@@ -1,6 +1,20 @@
 import path from 'path'
+import fileExists from './utils/fileExists'
 
-const modulePath = m => path.join(__dirname, '../node_modules', m)
+// Installed inside
+let moduleDir = path.join(__dirname, '../node_modules')
+
+// Installed as devDependency
+if (!fileExists(moduleDir)) {
+  moduleDir = path.join(__dirname, '../../../node_modules')
+}
+
+// Last try: current working directory
+if (!fileExists(moduleDir)) {
+  moduleDir = path.join(process.cwd(), 'node_modules')
+}
+
+const modulePath = m => path.join(moduleDir, m)
 
 export default {
   presets: [
