6d9601c4266a1236e67959beae066b31a169511f
eliot-akira <me@eliotakira.com>
2017-01-03 18:42:21 +0100

    examples

---
+++ examples/fullstack/src/server/index.js
@@ -0,0 +1,12 @@
+import path from 'path'
+import express from 'express'
+
+const app = express()
+const client = path.join(process.cwd(), '/build/client')
+const port = process.env.PORT || 3000
+
+app.use(express.static(client))
+
+app.listen(port, () =>
+  console.log(`Serving from port ${port}`)
+)
---
+++ examples/server/src/index.js
@@ -0,0 +1,13 @@
+import path from 'path'
+import express from 'express'
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('*', (req, res) => {
+  res.send('Hello from server')
+})
+
+app.listen(port, () =>
+  console.log(`Serving from port ${port}`)
+)
